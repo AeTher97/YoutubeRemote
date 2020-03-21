7536f9b8d87e1c68c5d90c53bd324e7abfd36f73
Michał Woźniak <michael93509@gmail.com>
2020-03-21 16:28:10 +0100

    Created folders

---
+++ YouTubeRemoteBackend/src/main/java/com/urzednicza/youtuberemotebackend/YouTubeRemoteApplication.java
@@ -0,0 +1,12 @@
+package com.urzednicza.youtuberemotebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeRemoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouTubeRemoteApplication.class, args);
+    }
+}
